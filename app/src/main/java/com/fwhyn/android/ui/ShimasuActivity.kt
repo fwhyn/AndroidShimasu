81585a53e6e5a4a9b32580ef22c032bc84fa2031
Yana Wahyuna <yana.wahyuna@epson.co.id>
2023-02-15 15:09:05 +0700

    clean code

--- app/src/main/java/com/fwhyn/android/ui/ShimasuActivity.kt
+++ app/src/main/java/com/fwhyn/android/ui/ShimasuActivity.kt
@@ -1,9 +1,10 @@
-package com.fwhyn.android
+package com.fwhyn.android.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fwhyn.android.R
 
-class MainActivity : AppCompatActivity() {
+class ShimasuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
