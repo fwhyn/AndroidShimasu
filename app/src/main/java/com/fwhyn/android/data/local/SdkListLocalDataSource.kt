1670caf900aa95158759b82e2dfb527e0f48571c
Yana Wahyuna <yana.wahyuna@epson.co.id>
2023-02-22 11:05:08 +0700

    Test Service

--- app/src/main/java/com/fwhyn/android/data/local/SdkListLocalDataSource.kt
+++ app/src/main/java/com/fwhyn/android/data/local/SdkListLocalDataSource.kt
@@ -2,9 +2,11 @@ package com.fwhyn.android.data.local
 
 import com.fwhyn.android.data.model.SdkModel
 import com.fwhyn.android.ui.feature.main.ShimasuActivity
+import com.fwhyn.servicetest.ServiceTestActivity
 
 class SdkListLocalDataSource {
     val sdkModelList = listOf(
         SdkModel("Shimasu Activity", ShimasuActivity::class.java),
+        SdkModel("Service Test", ServiceTestActivity::class.java),
     )
 }
---
+++ servicetest/src/main/java/com/fwhyn/servicetest/ServiceTestActivity.java
@@ -0,0 +1,14 @@
+package com.fwhyn.servicetest;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ServiceTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_test);
+    }
+}
